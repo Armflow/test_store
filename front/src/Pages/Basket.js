4d58e43424a0063a5cbfa3c01125addbed2f2bd8
Yefim Yakir <65903951+Armflow@users.noreply.github.com>
2021-06-07 21:34:25 +0300

    Update Basket.js

--- front/src/Pages/Basket.js
+++ front/src/Pages/Basket.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './styles.css'
 
 export default function Basket(props) {
+
+    const [ ,setCandies] = React.useState(props.state.candies)
+
+    function  deleteCandy(id) {
+        setCandies(props.state.candies.map(candy =>{
+          if (candy.ordered === true && candy.id === id){
+            candy.ordered = false;
+          }
+          return candy
+        })
+       )
+      }
+
     return (
         <div className='app'>
             <h3>Basket content ({(props.state.candies.filter( ({ordered}) => ordered === true)).length} items):</h3>
@@ -10,7 +23,18 @@ export default function Basket(props) {
                     props.state.candies.map((candy, index) => {
                     return (
                         <div>
-                            {candy.ordered === true ? <div className='list'>{candy.name}&nbsp;-&nbsp;${candy.price}</div> : null}
+                            {candy.ordered === true ? 
+                                <div className='list'>
+                                    <tr>
+                                        <td width='350px'>
+                                            {candy.name}&nbsp;-&nbsp;${candy.price}
+                                        </td>
+                                        <td>
+                                            <button onClick={() => deleteCandy(candy.id)}>{'Delete item'}</button>
+                                        </td>
+                                    </tr>
+                                </div> : null
+                            }
                         </div>
                 )
             }) 
